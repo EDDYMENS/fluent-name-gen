b2bc7fe4b5044dae6723f275844c6f8920a90d0b
eddymens <kwasiamantin@gmail.com>
2016-09-28 05:06:23 +0000

    init commit

---
+++ generator.php
@@ -0,0 +1,35 @@
+<?php
+
+$adjs = [
+    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+    "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
+    "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
+    "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
+    "wandering", "withered", "wild", "black", "young", "holy", "solitary",
+    "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
+    "polished", "ancient", "purple", "lively", "nameless"
+ ];
+
+$nouns = [
+    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
+    "frog", "smoke", "star"
+];
+
+function generate_name($adjs, $nouns, $separator='-') {
+    $num  = intval(rand(2,3)*pow(5,5));
+    $adj  = $adjs[array_rand($adjs, 1)];
+    $noun = $nouns[array_rand($nouns, 1)];
+    return $adj.$separator.$noun.$separator.$num;
+}
+
+echo generate_name($adjs, $nouns);
+
